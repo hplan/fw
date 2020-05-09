293d8fdcf77fde3a210796aa5330764c71df485a
hplan <hplan@grandstream.cn>
2020-05-09 20:37:22 +0800

    add rgba to rgb srcipts

---
+++ monitor/rgba2rgb.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+
+from PIL import Image
+
+if __name__ == '__main__':
+    img = Image.open('/home/hplan/project/alpaca7/android/hardware/intel/kernelflinger/libkernelflinger/res/images/oem_99.png')
+    print(img.mode)
+    img = img.convert('RGB')
+    img.save('/home/hplan/project/alpaca7/android/hardware/intel/kernelflinger/libkernelflinger/res/images/oem_99_2.png')
+    print(img.mode)
